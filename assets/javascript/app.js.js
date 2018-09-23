58a2424575c7f0d8a38a4a7a65f1ae708e4a8304
Stephen Bissett <steve.bissett5@gmail.com>
2018-09-23 12:07:13 -0600

    Update2Sunday

--- assets/javascript/app.js.js
+++ assets/javascript/app.js.js
@@ -29,3 +29,31 @@ var questions = [{
   answers: [" The Red Arrow   ", " Arrowhead   ", " Mr Robinson   ", " Yaka Arrow   ", " Whistler   "],
   correctAnswer: "Yaka Arrow   "
 }];
+
+var game = {
+    correct: 0,
+    incorrect: 0,
+    // seconds
+    counter: 30,
+    countdown: function () {
+        // created counter function that will 'start' below
+        game.counter--;
+        $('#counter').html(game.counter);
+        if (game.counter <= 0) {
+            console.log("Time is up");
+            // this method will run when game is done
+            game.done();
+        }
+    },
+    start: function () {
+        // insert time, 1000seconds
+        timer = setInterval(game.countdown,1000);
+        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">30</span> Seconds</h2>');
+        $('#start').remove();
+        for (var i = 0; i < questions.length; i++) {
+            // append to subwrapper
+            $('#subwrapper').append('<h2>' + questions[i].question + '</h2');
+            // subloop
+            for (var j = 0; j < questions[i].answers.length; j++) {
+                $("#subwrapper").append("<input type='radio' name='question-" +i+ "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j]);
+            }
